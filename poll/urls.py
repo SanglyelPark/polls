b2a327cdd9f2d4d4dacbe33cc0934ca9023748ca
slpark8888 <slpark8888@gmail.com>
2022-02-25 12:49:08 +0900

    poll

--- poll/urls.py
+++ poll/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'poll'  # 네임 스페이스(소속)
+
 urlpatterns = [
-    path('', views.index),  # 127.0.0.1/poll/
-    path('cart/', views.cart)
+    path('', views.index, name='index'),  # 127.0.0.1/poll/
+    # path('cart/', views.cart),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/vote/', views.vote, name='vote'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from poll.models import Question
+
+
 def index(request):
+    question_list = Question.objects.all()
     # return HttpResponse("<h1>안녕~ Django!!</h1>")
-    return render(request, 'poll/index.html')
+    return render(request, 'poll/index.html', {'question_list':question_list} )
+
+def detail(request, pk):
+    question = Question.objects.get(id=pk)
+    return render(request,'poll/detail.html', {'question':question})
+
+def vote(request, pk):
+    pass
 
 def cart(request):
     cart = "계란"
